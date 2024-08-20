a334417e2d23005de9c42613ea380acb8e019736
LewisJassy <jassylewis70@gmail.com>
2024-08-20 23:26:52 +0300

    create a readme

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-"""Type-annotated function add that takes a float a and a float b as arguments and return a float."""
-def add(a: float, b: float) -> float:
-    return a + b
